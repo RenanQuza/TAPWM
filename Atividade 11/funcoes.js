27c8645f4dd04b3969b991b76c728e660ecea0e9
Renan Queiroz de Souza <99503650+RenanQuza@users.noreply.github.com>
2023-10-15 23:13:44 -0300

    Add files via upload

---
+++ Atividade 11/funcoes.js
@@ -0,0 +1,9 @@
+function MaiorNumero(num1, num2, num3, num4) {
+    return Math.max(num1, num2, num3, num4);
+  }
+  
+  function NumerosCrescente(num1, num2, num3, num4) {
+    const numeros = [num1, num2, num3, num4];
+    numeros.sort((a, b) => a - b);
+    return numeros;
+  }
